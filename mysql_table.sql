42894431dadf113bcbcfaf968a476290700b2ce7
xAlineOliveira <2024391@student.cct.ie>
2024-12-22 10:09:55 +0000

    Update mysql_table.sql
    Eircode size updated to 7 and additional user for myself.

--- mysql_table.sql
+++ mysql_table.sql
@@ -9,14 +9,15 @@ CREATE TABLE IF NOT EXISTS userdata (
 	age INT,
 	email VARCHAR(255),
 	phone VARCHAR(20),
-	eircode VARCHAR(6),
+	eircode VARCHAR(7),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 INSERT INTO userdata (first_name, last_name, age, email, phone, eircode)
 VALUES 
-('Alexandre', 'Casaroes', 30, '2024672@student.cct.ie', '0000000000', 'D07A0E1');
+('Alexandre', 'Casaroes', 30, '2024672@student.cct.ie', '0000000000', 'D07A0E1'),
+('Aline', 'Oliveira', 33, '2024391@student.cct.ie', 0000000001, 'D07FX60');
 
 SHOW DATABASES;
 
